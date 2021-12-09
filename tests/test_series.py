979f1e6f65d00b9ba90c2be8219132ceb5bccfc9
Chris Rarig <cwrarig20@gmail.com>
2021-12-08 21:31:08 -0600

    added sum function

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,10 +1,12 @@
 import pytest
-from math_series.series import fibonacci,lucas
+from math_series.series import fibonacci,lucas,sum_series
 
+# fibonacci tests
 def test_one():
     actual = fibonacci(1)
     expected = 1
     assert actual == expected
+
 
 def test_two():
     actual = fibonacci(2)
@@ -21,6 +23,7 @@ def test_eight():
     expected = 21
     assert actual == expected
 
+# Lucas tests
 
 def test_luctwo():
     actual = lucas(2)
@@ -30,4 +33,21 @@ def test_luctwo():
 def test_lucfive():
     actual = lucas(5)
     expected = 11
+    assert actual == expected
+
+# Sum Function Tests
+
+def test_sumfib():
+    actual = sum_series(2)
+    expected = 1
+    assert actual == expected
+
+def test_sumlucas():
+    actual = sum_series(2, 2,1)
+    expected = 3
+    assert actual == expected
+
+def test_sum6():
+    actual = sum_series(6, 3,5)
+    expected = 55
     assert actual == expected
